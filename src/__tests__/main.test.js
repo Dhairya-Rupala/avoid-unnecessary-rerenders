e47622695d694d6f7aee16642b639067896aaea0
Dhairya Rupala <dhairyakumar.rupala@sprinklr.com>
2024-09-01 22:57:18 +0530

    Add tests

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -1,6 +1,6 @@
 import App from '../App';
 import { within } from '@testing-library/dom';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
@@ -26,13 +26,11 @@ test('1', async () => {
   userEvent.clear(roleInput);
   userEvent.type(roleInput, 'QA');
 
-  const businessPreviewCard = getByTestId('business-card-preview');
+  const resumePreview = getByTestId('resume-preview');
 
-  await waitFor(() => {
-    within(businessPreviewCard).getByText('Alex Grey');
-    within(businessPreviewCard).getByText('Company: Tech Corp');
-    within(businessPreviewCard).getByText('Role: QA');
-  });
+  within(resumePreview).getByText('Alex Grey');
+  within(resumePreview).getByText('Company: Tech Corp');
+  within(resumePreview).getByText('Role: QA');
 });
 
 test('2', () => {
@@ -43,13 +41,13 @@ test('2', () => {
   const firstNameInput = getByTestId('first-name');
   userEvent.type(firstNameInput, 'x');
 
-  expect(consoleSpy).toHaveBeenCalledTimes(4);
+  expect(consoleSpy).toHaveBeenCalledTimes(3);
 
   consoleSpy.mockClear();
   const companyInput = getByTestId('company');
   userEvent.type(companyInput, 'y');
 
-  expect(consoleSpy).toHaveBeenCalledTimes(4);
+  expect(consoleSpy).toHaveBeenCalledTimes(3);
 });
 
 test('3', () => {
@@ -78,4 +76,121 @@ test('4', () => {
 
   userEvent.click(changeHeaderColorButton);
   expect(header).toHaveAttribute('style', 'background-color: dodgerBlue;');
+});
+
+test('5', () => {
+  const { getByTestId } = render(<App />);
+
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  const form = getByTestId('form');
+
+  userEvent.type(skillNameInput, 'C++');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'Python');
+  userEvent.selectOptions(efficiencySelector, '7');
+  userEvent.click(addSkillButton);
+
+  expect(within(form).getByText('C++ (1)'));
+  expect(within(form).getByText('Python (7)'));
+
+  userEvent.click(submitSkillsButton);
+
+  const resumePreview = getByTestId('resume-preview');
+  expect(within(resumePreview).getByText('C++ (1)'));
+  expect(within(resumePreview).getByText('Python (7)'));
+
+  const firstSkill = within(resumePreview).getByTestId('skill-tag-0');
+  const secondSkill = within(resumePreview).getByTestId('skill-tag-1');
+
+  expect(within(firstSkill).getByText('Python (7)'));
+  expect(within(secondSkill).getByText('C++ (1)'));
+});
+
+test('6', () => {
+  const { getByTestId } = render(<App />);
+  consoleSpy.mockClear();
+
+  const skillNameInput = getByTestId('skill-name-input');
+  userEvent.type(skillNameInput, 'x');
+
+  expect(consoleSpy).toHaveBeenCalledTimes(1);
+
+  const efficiencySelector = getByTestId('efficiency-selector');
+  userEvent.selectOptions(efficiencySelector, '3');
+
+  expect(consoleSpy).toHaveBeenCalledTimes(2);
+
+  consoleSpy.mockClear();
+  const addSkillButton = getByTestId('add-skill-button');
+  userEvent.click(addSkillButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(2);
+
+  consoleSpy.mockClear();
+  const clearSkillsButton = getByTestId('clear-skills-button');
+  userEvent.click(clearSkillsButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(1);
+});
+
+test('7', () => {
+  const { getByTestId } = render(<App />);
+
+  const firstNameInput = getByTestId('first-name');
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  userEvent.type(skillNameInput, 'Javascript');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+  userEvent.click(submitSkillsButton);
+  consoleSpy.mockClear();
+
+  userEvent.type(firstNameInput, 'D');
+  expect(consoleSpy).toHaveBeenCalledTimes(3);
+});
+
+test('8', () => {
+  const { getByTestId } = render(<App />);
+
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const clearSkillsButton = getByTestId('clear-skills-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  userEvent.type(skillNameInput, 'Jest');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'Java');
+  userEvent.selectOptions(efficiencySelector, '2');
+  userEvent.click(addSkillButton);
+  consoleSpy.mockClear();
+
+  userEvent.click(submitSkillsButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(4);
+  userEvent.click(clearSkillsButton);
+
+  userEvent.type(skillNameInput, 'Java');
+  userEvent.selectOptions(efficiencySelector, '2');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'Jest');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  consoleSpy.mockClear();
+  userEvent.click(submitSkillsButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(2);
 });
--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -1,6 +1,6 @@
 import App from '../App';
 import { within } from '@testing-library/dom';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
@@ -14,11 +14,8 @@ test('Company input field should function properly', async () => {
   userEvent.clear(companyInput);
   userEvent.type(companyInput, 'Test Company');
 
-  const businessPreviewCard = getByTestId('business-card-preview');
-
-  await waitFor(() => {
-    within(businessPreviewCard).getByText('Company: Test Company');
-  });
+  const resumePreview = getByTestId('resume-preview');
+  within(resumePreview).getByText('Company: Test Company');
 });
 
 test('Changing header color should change the background color of the header', () => {
@@ -47,4 +44,94 @@ test('Changing header color should only re-render the header', () => {
   userEvent.click(changeHeaderColorButton);
 
   expect(consoleSpy).toHaveBeenCalledTimes(1);
+});
+
+test('Adding skills and pressing Done button should show the skills in the order with highest efficiency first in resume preview', () => {
+  const { getByTestId } = render(<App />);
+
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  const form = getByTestId('form');
+
+  userEvent.type(skillNameInput, 'skill 1');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'skill 2');
+  userEvent.selectOptions(efficiencySelector, '2');
+  userEvent.click(addSkillButton);
+
+  expect(within(form).getByText('skill 1 (1)'));
+  expect(within(form).getByText('skill 2 (2)'));
+
+  userEvent.click(submitSkillsButton);
+
+  const resumePreview = getByTestId('resume-preview');
+  expect(within(resumePreview).getByText('skill 1 (1)'));
+  expect(within(resumePreview).getByText('skill 2 (2)'));
+
+  const firstSkill = within(resumePreview).getByTestId('skill-tag-0');
+  const secondSkill = within(resumePreview).getByTestId('skill-tag-1');
+
+  expect(within(firstSkill).getByText('skill 2 (2)'));
+  expect(within(secondSkill).getByText('skill 1 (1)'));
+});
+
+test('If we have added skills and if we change any other input it should only re-render the component which are required to re-render', () => {
+  const { getByTestId } = render(<App />);
+
+  const firstNameInput = getByTestId('first-name');
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  userEvent.type(skillNameInput, 'skill 1');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+  userEvent.click(submitSkillsButton);
+  consoleSpy.mockClear();
+
+  userEvent.type(firstNameInput, 'D');
+  expect(consoleSpy).toHaveBeenCalledTimes(3);
+});
+
+test('If we enter the skills in the different order with the same efficiency it should not re-render the Resume Preview', () => {
+  const { getByTestId } = render(<App />);
+
+  const skillNameInput = getByTestId('skill-name-input');
+  const efficiencySelector = getByTestId('efficiency-selector');
+  const addSkillButton = getByTestId('add-skill-button');
+  const clearSkillsButton = getByTestId('clear-skills-button');
+  const submitSkillsButton = getByTestId('submit-skills-button');
+
+  userEvent.type(skillNameInput, 'skill 1');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'skill 2');
+  userEvent.selectOptions(efficiencySelector, '2');
+  userEvent.click(addSkillButton);
+  consoleSpy.mockClear();
+
+  userEvent.click(submitSkillsButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(4);
+  userEvent.click(clearSkillsButton);
+
+  userEvent.type(skillNameInput, 'skill 2');
+  userEvent.selectOptions(efficiencySelector, '2');
+  userEvent.click(addSkillButton);
+
+  userEvent.type(skillNameInput, 'skill 1');
+  userEvent.selectOptions(efficiencySelector, '1');
+  userEvent.click(addSkillButton);
+
+  consoleSpy.mockClear();
+  userEvent.click(submitSkillsButton);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(2);
 });
